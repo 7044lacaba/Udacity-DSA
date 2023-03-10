efd25f79125ffac370a0e70e7fbf660d86c432c1
7044lacaba <johnlacaba6@gmail.com>
2023-03-09 18:41:26 -0800

    init 12

---
+++ 11-tree-traversal.py
@@ -0,0 +1,15 @@
+"""
+Tree:
+        A
+    /       \
+    B       C
+/       \       \
+D       E       F
+
+
+Level-Order (BFS) - A, B, C, D, E, F
+Pre-Order (DFS) - A, B, D, E, C, F
+In-Order (DFS) - D, B, E, A, C, F
+Post-Order (DFS) - D, E, B, F, C, A
+
+"""
---
+++ 12-binary-tree.py
@@ -0,0 +1,36 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def search(self, find_val):
+        """Return True if the value
+        is in the tree, return
+        False otherwise."""
+        return False
+
+    def print_tree(self):
+        """Print out all tree nodes
+        as they are visited in
+        a pre-order traversal."""
+        return ""
+
+    def preorder_search(self, start, find_val):
+        """Helper method - use this to create a 
+        recursive search solution."""
+        return False
+
+    def preorder_print(self, start, traversal):
+        """Helper method - use this to create a 
+        recursive print solution."""
+        return traversal
+
+
+# Set up tree
+tree = BinaryTree(1)
+tree.root.left = Node(2)
